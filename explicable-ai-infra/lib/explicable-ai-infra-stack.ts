92e19c8c44e805a997f012442ed2e7907d78f2bc
ben <benoit.paris.753@gmail.com>
2024-04-29 13:50:35 +0200

    Imported S3 redirect and serving, email

--- explicable-ai-infra/lib/explicable-ai-infra-stack.ts
+++ explicable-ai-infra/lib/explicable-ai-infra-stack.ts
@@ -1,16 +1,106 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ses from 'aws-cdk-lib/aws-ses';
+import {Construct} from 'constructs';
 
 export class ExplicableAiInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Resources
+    const s3BucketExplicableaiServe = new s3.CfnBucket(this, 'S3Bucket-explicableai-serve', {
+      publicAccessBlockConfiguration: {
+        restrictPublicBuckets: false,
+        ignorePublicAcls: false,
+        blockPublicPolicy: false,
+        blockPublicAcls: false,
+      },
+      bucketName: 'explicable.ai',
+      ownershipControls: {
+        rules: [
+          {
+            objectOwnership: 'BucketOwnerPreferred',
+          },
+        ],
+      },
+      bucketEncryption: {
+        serverSideEncryptionConfiguration: [
+          {
+            bucketKeyEnabled: false,
+            serverSideEncryptionByDefault: {
+              sseAlgorithm: 'AES256',
+            },
+          },
+        ],
+      },
+      websiteConfiguration: {
+        indexDocument: 'index.html',
+        errorDocument: '404.html',
+      },
+      versioningConfiguration: {
+        status: 'Enabled',
+      },
+      tags: [
+        {
+          value: 'hosting',
+          key: 'explainable.ai',
+        },
+      ],
+    });
+    s3BucketExplicableaiServe.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ExplicableAiInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3BucketWwwexplicableaiRedirect = new s3.CfnBucket(this, 'S3Bucket-wwwexplicableai-redirect', {
+      websiteConfiguration: {
+        redirectAllRequestsTo: {
+          hostName: 'explicable.ai',
+        },
+      },
+      publicAccessBlockConfiguration: {
+        restrictPublicBuckets: false,
+        ignorePublicAcls: false,
+        blockPublicPolicy: false,
+        blockPublicAcls: false,
+      },
+      bucketName: 'www.explicable.ai',
+      versioningConfiguration: {
+        status: 'Enabled',
+      },
+      ownershipControls: {
+        rules: [
+          {
+            objectOwnership: 'BucketOwnerPreferred',
+          },
+        ],
+      },
+      bucketEncryption: {
+        serverSideEncryptionConfiguration: [
+          {
+            bucketKeyEnabled: false,
+            serverSideEncryptionByDefault: {
+              sseAlgorithm: 'AES256',
+            },
+          },
+        ],
+      },
+    });
+    s3BucketWwwexplicableaiRedirect.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
+
+    const sesEmailIdentityContactExplicableAi = new ses.CfnEmailIdentity(this, 'SESEmailIdentitycontactexplicableai', {
+      dkimAttributes: {
+        signingEnabled: false,
+      },
+      feedbackAttributes: {
+        emailForwardingEnabled: true,
+      },
+      emailIdentity: 'contact@explicable.ai',
+      dkimSigningAttributes: {
+        nextSigningKeyLength: 'RSA_1024_BIT',
+      },
+      mailFromAttributes: {
+        behaviorOnMxFailure: 'USE_DEFAULT_VALUE',
+      },
+    });
+    sesEmailIdentityContactExplicableAi.cfnOptions.deletionPolicy = cdk.CfnDeletionPolicy.RETAIN;
+
   }
 }
